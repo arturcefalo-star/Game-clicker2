5254872e537c75de8aab11830d5382869cddc4ef
arturcefalo-star <arturcefalo@gmail.com>
2026-08-19 21:15:52 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("")
 
-if st.session.state_pontos not in st.session.state:
+if "pontos" not in st.session.state:
   st.session.state_pontos = 67
 
 st.write("pontos")
