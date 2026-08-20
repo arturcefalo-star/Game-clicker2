95c7687da9cdb34cb3a6327cb8c964da6cfc6bb1
arturcefalo-star <arturcefalo@gmail.com>
2026-08-19 21:36:44 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,13 @@ import streamlit as st
 st.title("")
 
 if "pontos" not in st.session_state:
-  st.session_state.pontos = 1
+  st.session_state.pontos = 0
 if "poder_c" not in st.session_state:
   st.session_state.poder_c = 1
   
 st.write(f"**Pontos:** {st.session_state.pontos:,}")
 
-if st.button("test"):
+if st.button("          CLIQUE AQUI         "):
    st.session_state.pontos += st.session_state.poder_c
   
 
