6557ebbfa9475311ac164ff052687b3d7cf89742
arturcefalo-star <arturcefalo@gmail.com>
2026-08-19 21:40:30 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,17 @@ if "pontos" not in st.session_state:
   st.session_state.pontos = 0
 if "poder_c" not in st.session_state:
   st.session_state.poder_c = 1
+if "poder_b" not in st.session_state:
+  st.session_state.poder_b = 1
   
 st.write(f"**Pontos:** {st.session_state.pontos:,}")
 
 if st.button("          CLIQUE AQUI         "):
    st.session_state.pontos += st.session_state.poder_c
+
+if st.button("Melhoria = 100 Pts"):
+  if "pontos" >= 100:
+     st.session_state.poder_c += st.session_state.poder_b
+  
   
 
