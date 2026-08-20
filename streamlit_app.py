93f0899713798307435334e960f9118aee2a5046
arturcefalo-star <arturcefalo@gmail.com>
2026-08-19 21:43:03 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ if st.button("          CLIQUE AQUI         "):
    st.session_state.pontos += st.session_state.poder_c
 
 if st.button("Melhoria = 100 Pts"):
-  if "st.session_state.pontos" >= 100:
+  if st.session_state.pontos >= 100:
      st.session_state.poder_c += st.session_state.poder_b
   
   
